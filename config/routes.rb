324e2a4a06187a4132ab958f071993d26a28c7b1
KenjiOyamada <oyaken0717@yahoo.co.jp>
2019-04-24 20:42:25 +0900

    [add] ログイン機能

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   root to: "homes#top"
   get "/about", to: "homes#about"
+  resources :sessions, only: [:new, :create, :show, :destroy]
 
   resources :groups do
     resources :posts do
