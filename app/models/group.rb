2fa8edda53d261a71dd6f807a9bfbf1d90f180cb
KenjiOyamada <oyaken0717@yahoo.co.jp>
2019-04-24 11:41:32 +0900

    [add] user機能scssまで実装

--- app/models/group.rb
+++ app/models/group.rb
@@ -1,6 +1,6 @@
 class Group < ApplicationRecord
   belongs_to :user
-  validates :name, presence: true
+  validates :name, presence: true, length:  { maximum: 30 }
   has_many :posts
   has_many :members, dependent: :destroy
   has_many :member_users, through: :members, source: :user
--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,5 @@ Rails.application.routes.draw do
   get "/about", to: "homes#about"
 
   resources :groups
+  resources :users
 end
