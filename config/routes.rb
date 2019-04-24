d4de997838a8fa0a6a9802710c45c18b2e45f29e
KenjiOyamada <oyaken0717@yahoo.co.jp>
2019-04-24 13:56:20 +0900

    [add] postのCRUD機能作成

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,13 @@ Rails.application.routes.draw do
   root to: "homes#top"
   get "/about", to: "homes#about"
 
-  resources :groups
+  resources :groups do
+    resources :posts do
+      collection do
+        post :confirm
+      end
+      # resources :comments
+    end
+  end
   resources :users
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_04_23_120030) do
+ActiveRecord::Schema.define(version: 2019_04_24_040322) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "groups", force: :cascade do |t|
     t.string "name", null: false
+  end
+
+  create_table "posts", force: :cascade do |t|
+    t.text "title", null: false
+    t.text "content", default: "", null: false
   end
 
   create_table "users", force: :cascade do |t|
