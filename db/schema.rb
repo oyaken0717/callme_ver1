54019c4dbe67356a98bb601dcd1bd8fa43a36fc1
KenjiOyamada <oyaken0717@yahoo.co.jp>
2019-04-25 14:07:42 +0900

    [add] userにimage追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_04_25_042143) do
+ActiveRecord::Schema.define(version: 2019_04_25_050139) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -48,6 +48,7 @@ ActiveRecord::Schema.define(version: 2019_04_25_042143) do
     t.string "name", null: false
     t.string "password_digest"
     t.string "email"
+    t.text "image"
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
