4ef580456c5853a27aa6e5bbc92cb56eca8a938d
KenjiOyamada <oyaken0717@yahoo.co.jp>
2019-04-29 13:02:11 +0900

    [add] 作成者がユーザーAであるタスクを作成しておく

--- spec/system/tasks_spec.rb
+++ spec/system/tasks_spec.rb
@@ -3,7 +3,8 @@ require 'rails_helper'
 describe '仕事管理機能', type: :system do
   describe '一覧表示機能' do
     before do
-      user_a = FactoryBot.create(:user, name: 'b@b.com', email: 'b@b.com', password: 'b@b.com')
+      user_b = FactoryBot.create(:user, name: 'b@b.com', email: 'b@b.com', password: 'b@b.com')
+      FactoryBot.create(:post, name: 'post2', user: user_b)
     end
 
     context '' do
