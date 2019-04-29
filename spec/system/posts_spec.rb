b0e4f5c0479173be087238ac08fc6d313f48c8bf
KenjiOyamada <oyaken0717@yahoo.co.jp>
2019-04-29 17:44:16 +0900

    [add] グループに参加していないユーザーは仕事が表示されない

--- spec/system/posts_spec.rb
+++ spec/system/posts_spec.rb
@@ -29,17 +29,20 @@ describe '仕事管理機能', type: :system do
       end
     end
 
-    content 'ユーザー「C」がログインしている時' do
+    context 'ユーザー「C」がログインしている時' do
       before do
         FactoryBot.create(:user, name: 'c@c.com', email: 'c@c.com', password: 'c@c.com')
         visit new_session_path
         fill_in 'session_email', with: 'c@c.com'
         fill_in 'session_password', with: 'c@c.com'
         click_button 'ログイン'
+
+        click_on '全グループ一覧画面'
+        page.first(".group-item__body-link").click_link
       end
 
       it 'ユーザー『B』が作成したタスクが表示されない' do
-        expect(page).to have_no_content ''
+        expect(page).to have_no_content 'post2'
       end
     end
 
