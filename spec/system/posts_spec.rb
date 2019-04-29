b699708ec04e970ef1ef6b9495ff8b96fe91e563
KenjiOyamada <oyaken0717@yahoo.co.jp>
2019-04-29 13:29:35 +0900

    [add] 作成済みのタスクの名称が画面上に表示されていることを確認

--- spec/system/posts_spec.rb
+++ spec/system/posts_spec.rb
@@ -4,20 +4,21 @@ describe '仕事管理機能', type: :system do
   describe '一覧表示機能' do
     before do
       user_b = FactoryBot.create(:user, name: 'b@b.com', email: 'b@b.com', password: 'b@b.com')
-      FactoryBot.create(:post, name: 'post2', user: user_b)
+      FactoryBot.create(:post, title: 'post2', user: user_b)
+      FactoryBot.create(:group, name: 'group2')
     end
 
     context 'ユーザーBがログインしている時' do
       before do
-        visit login_path
+        visit new_session_path
         fill_in 'メールアドレス', with: 'b@b.com'
         fill_in 'パスワード', with: 'b@b.com'
         click_button 'ログイン'
       end
     end
 
-    it '' do
-
+    it 'ユーザーBが作成したタスクが表示される' do
+      expect(page).to have_content 'post2'
     end
   end
 end
