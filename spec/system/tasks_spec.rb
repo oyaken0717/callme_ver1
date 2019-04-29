44067fd9ba601fea4937df960f7ab20ddf894280
KenjiOyamada <oyaken0717@yahoo.co.jp>
2019-04-29 12:51:54 +0900

    [add] task一覧表示機能のSystem Spec

---
+++ spec/system/tasks_spec.rb
@@ -0,0 +1,19 @@
+require 'rails_helper'
+
+describe 'タスク管理機能', type: :system do
+  describe '一覧表示機能' do
+    before do
+
+    end
+
+    context '' do
+      before do
+
+      end
+    end
+
+    it '' do
+      
+    end
+  end
+end
