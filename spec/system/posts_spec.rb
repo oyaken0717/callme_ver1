615ab3738088388da82a5f562d88478090afbc4a
KenjiOyamada <oyaken0717@yahoo.co.jp>
2019-04-29 16:05:46 +0900

    [add] グループBでグループに参加する

--- spec/system/posts_spec.rb
+++ spec/system/posts_spec.rb
@@ -4,21 +4,29 @@ describe '仕事管理機能', type: :system do
   describe '一覧表示機能' do
     before do
       user_b = FactoryBot.create(:user, name: 'b@b.com', email: 'b@b.com', password: 'b@b.com')
-      FactoryBot.create(:post, title: 'post2', user: user_b)
-      FactoryBot.create(:group, name: 'group2')
+      group_2 = FactoryBot.create(:group, name: 'group2')
+      post_2 = FactoryBot.create(:post, title: 'post2', user: user_b, group: group_2)
+
     end
 
-    context 'ユーザーBがログインしている時' do
+    context 'ユーザーBがログインし、グループ2に所属しているとき' do
       before do
         visit new_session_path
-        fill_in 'メールアドレス', with: 'b@b.com'
-        fill_in 'パスワード', with: 'b@b.com'
+        fill_in 'session_email', with: 'b@b.com'
+        fill_in 'session_password', with: 'b@b.com'
         click_button 'ログイン'
-      end
-    end
 
-    it 'ユーザーBが作成したタスクが表示される' do
-      expect(page).to have_content 'post2'
+        click_on '全グループ一覧画面'
+        page.first(".group-item__body-link").click_link
+        click_on '参加する'
+        page.first(".group-item__body-link").click_link
+        expect(page).to have_content 'グループ名: group2'
+      end
+
+      it 'ユーザーBが作成したタスクが表示される' do
+        click_on '仕事一覧'
+        expect(page).to have_content 'post2'
+      end
     end
   end
 end
