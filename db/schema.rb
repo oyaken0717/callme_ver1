d70c8ec5fa2f4f91a2d43a73fbf15fb46fa7549b
KenjiOyamada <oyaken0717@yahoo.co.jp>
2019-05-04 16:55:11 +0900

    [add] conversation機能再実装

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_04_30_102612) do
+ActiveRecord::Schema.define(version: 2019_05_04_064515) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
