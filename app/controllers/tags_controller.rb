a730d73a95cca4d9ea47abb5e0045c5368640901
KenjiOyamada <oyaken0717@yahoo.co.jp>
2019-05-16 17:24:20 +0900

    [fix] タグ作成エラー時の遷移先変更

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -2,13 +2,18 @@ class TagsController < ApplicationController
   def create
     @tag = Tag.new(tag_params)
     if @tag.save
-      redirect_to groups_path, notice: "タグを作成しました。"
+      redirect_to group_posts_path, notice: "タグを作成しました。"
     else
       @groups = Group.all
+      # @group = Group.find_by(id:[:group_id])
+      @group = Group.find(params[:group_id])
+
       # @group = Group.find_by(id: params[:group_id])
       # @posts = Post.where(group_id: Group.find_by(id: params[:group_id]))
-      flash.now[:notice] = "もう一度チャレンジしてみてください！！"
-      render 'groups/index'
+      # flash.now[:notice] = "もう一度チャレンジしてみてください！！"
+      # render 'groups/index'
+      # render group_posts_path(@group)
+      redirect_to group_posts_path, notice: "もう一度チャレンジしてみてください！！"
     end
   end
 
