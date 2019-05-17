74e8eb0f51f80191a39ed9b5e9727bb56b3a5928
KenjiOyamada <oyaken0717@yahoo.co.jp>
2019-05-17 12:55:18 +0900

    [fix] ログインエラー時のメッセージ表示方法の質問

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,14 +4,31 @@ class SessionsController < ApplicationController
     redirect_to users_path if logged_in?
   end
 
+
+  # def create
+  #   user = User.find_by(email: params[:session][:email].downcase)
+  #   if user && user.authenticate(params[:session][:password])
+  #     session[:user_id] = user.id
+  #     flash[:notice] = "ログインしました"
+  #     redirect_to user_path(user.id)
+  #   else
+  #     flash.now[:danger] = "ログイン失敗"
+  #     render "new"
+  #   end
+  # end
+
   def create
-    user = User.find_by(email: params[:session][:email].downcase)
-    if user && user.authenticate(params[:session][:password])
+    # @user = User.find_by(email: params[:session][:email].downcase)
+    # @user = User.find_by(email: params[:session][:email].downcase)
+    @user = User.find_by(params.require(:session).permit(:email, :password))
+    # Blog.create(params.require(:blog).permit(:title, :content))
+    @user&.email = @user&.email&.downcase
+    if @user && @user.authenticate(params[:session][:password])
       session[:user_id] = user.id
       flash[:notice] = "ログインしました"
-      redirect_to user_path(user.id)
+      redirect_to user_path(@user.id)
     else
-      flash.now[:danger] = "ログイン失敗"
+      # flash.now[:danger] = "ログイン失敗"
       render "new"
     end
   end
@@ -20,5 +37,11 @@ class SessionsController < ApplicationController
     session.delete(:user_id)
     flash[:notice] = "ログアウトしました"
     redirect_to new_session_path
+  end
+
+  private
+
+  def session_params
+    params.require(:user).permit(:name, :email, :image, :image_cache, :password, :password_confirmation)
   end
 end
